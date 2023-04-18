6db500b6c403d73878e18a3b93a499ba438d65c2
RukNdf <48357253+RukNdf@users.noreply.github.com>
2023-04-18 09:22:10 -0300

    enemy hit

--- jogo/assets/hud/HealthBar.gd
+++ jogo/assets/hud/HealthBar.gd
@@ -1,25 +1,13 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play('damage')
 	$AnimationPlayer.pause()
 	pass # Replace with function body.
 
 
-var pressed = false
-var b = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_key_pressed(KEY_1):
-		if !pressed:
-			pressed = true
-			$AnimationPlayer.advance(0.1)
-	elif Input.is_key_pressed(KEY_2):
-		if !pressed:
-			pressed = true
-			$AnimationPlayer.advance(-0.1)
-	else:
-		pressed = false
-	pass
+var max
+func move(cur):
+	print(cur/max)
+	$AnimationPlayer.seek(1-(cur/max), true)
